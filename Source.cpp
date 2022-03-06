8a93d5bfd68a97d2e6a21d8933d4cdd6d0eb212f
dvn266 <100774510+dvn266@users.noreply.github.com>
2022-03-06 21:29:06 +0700

    Update Source.cpp
    source code

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,6 @@
 #include<iostream>
 using namespace std;
 int main() {
-<<<<<<< HEAD
-	cout << "Hello everybody..." << endl;
-=======
-	cout << "Hello earth" << endl;
->>>>>>> 239903187c840fa07cd73a971725a27dc690e2fd
+	cout << "Hello world" << endl;
 	return 0;
 }
