d4089cebc04db4c02afee10b86cf3422be6472b7
dvn266 <100774510+dvn266@users.noreply.github.com>
2022-03-07 07:45:09 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,6 @@
 #include<iostream>
 using namespace std;
 int main() {
-<<<<<<< HEAD
 	cout << "Hello everybody" << endl;
-=======
-	cout << "Hello Everone" << endl;
->>>>>>> c35028678100602385ff2405b026258def0fd2d2
 	return 0;
 }
